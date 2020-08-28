bd1cacdc379d17df60f0561f092403244f39336d
braday <braderson888@gmail.com>
2020-08-28 15:29:47 +1000

    adding the scaffolding

---
+++ src/java/ASD/Email/Controller/EmailLogic.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ASD.Email.Controller;
+
+import java.util.List;
+
+/**
+ *
+ * @author Patty
+ */
+public class EmailLogic {
+    String from;
+    List<String> to;
+    String subject;
+    String body;
+    
+
+    
+    
+    public static void SendEmail(String from, List<String> to, String subject, String body){
+        // some logic
+    }
+    
+}
+
