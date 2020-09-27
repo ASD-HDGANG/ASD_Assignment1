88ce40b6b0d34ac4fa9336f7f3e5adb352794d97
hugh <hdgangganggang@gmail.com>
2020-09-28 00:01:58 +0800

    s
    s

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -1,37 +1,37 @@
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author daohuxia
- */
-public class Tester {
-    
-    WebDriver dirver;
-    
-    @Given("^I have open browser$")
-    public void givenStatement()throws InstantiationException, IllegalAccessException{
-        Class<? extends WebDriver> driverClass=FirefoxDriver.class;
-        WebDriverManager.getInstance(driverClass).setup();
-        WebDriver driver = driverClass.newInstance();
-        driver.get("https://www.sina.com.cn/");
-        
-        System.out.println("Given statement executed successfully!");
-    }
-    
-    @When("^I visit website$")
-    public void whenStatement(){
-        driver.get("");
-    }
-    
-}
+//import cucumber.api.java.en.Given;
+//import cucumber.api.java.en.When;
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//
+///**
+// *
+// * @author daohuxia
+// */
+//public class Tester {
+//    
+//    WebDriver dirver;
+//    
+//    @Given("^I have open browser$")
+//    public void givenStatement()throws InstantiationException, IllegalAccessException{
+//        Class<? extends WebDriver> driverClass=FirefoxDriver.class;
+//        WebDriverManager.getInstance(driverClass).setup();
+//        WebDriver driver = driverClass.newInstance();
+//        driver.get("https://www.sina.com.cn/");
+//        
+//        System.out.println("Given statement executed successfully!");
+//    }
+//    
+//    @When("^I visit website$")
+//    public void whenStatement(){
+//        driver.get("");
+//    }
+//    
+//}
--- src/test/java/TripPlannerTest.java
+++ src/test/java/TripPlannerTest.java
@@ -1,3 +1,9 @@
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.junit.Cucumber;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +15,27 @@
  * @author daohuxia
  */
 public class TripPlannerTest {
-    
+@Given("^user navigates to SOFTWARETETINGHELP.COM$")
+public void navigatePage() {
+System.out.println("Cucumber executed Given statement");
 }
+@When("^trip logs in using Departure as \"(.*)\" and Destination \"(.*)\"$")
+public void search(String departure,String destination) {
+System.out.println("Departure is:"+ departure);
+System.out.println("Destination is:"+ destination);
+}
+@When("^click the Search button$")
+public void clickTheSearchButton() {
+System.out.println("Executing When statement");
+}
+@Then("^Google page should be displayed$")
+public void validatePage() {
+System.out.println("Executing Then statement");
+}
+@Then("^Google should be successful$")
+public void validateSearchSuccess() {
+System.out.println("Executing 2<sup>nd</sup> Then statement");
+}
+}
+    
+
